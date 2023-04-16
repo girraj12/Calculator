664d7bf8d0f0bf1e5795e0e70a8ca4251999f44a
girraj12 <72375025+girraj12@users.noreply.github.com>
2023-04-16 15:06:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;    
+       console.log('button text is',buttonText);
+       if(buttonText == 'x'){
+          buttonText='*';
+          screenValue+=buttonText;
+          screen.value=screenValue;
+        }
+        else if(buttonText == 'c'){
+             screenValue=" ";
+             screen.value=screenValue;
+         }
+         else if(buttonText == '='){
+            screen.value=eval(screenValue);
+         }
+         else{
+            console.log("\n 21buttonText",buttonText)
+            screenValue+=buttonText;
+            screen.value = screenValue;
+         }
+         console.log("\n screen",screen)
+
+    })
+}
